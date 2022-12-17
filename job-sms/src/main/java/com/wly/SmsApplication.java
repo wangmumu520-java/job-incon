823ea3a6f4a1268ad09e7bfd44c038655aa259de
wanglinyuan <w956322363@163.com>
2022-12-18 00:24:26 +0800

    完善SMS服务配置文件、注册到nacos服务注册中心

--- job-sms/src/main/java/com/wly/SmsApplication.java
+++ job-sms/src/main/java/com/wly/SmsApplication.java
@@ -3,13 +3,11 @@ package com.wly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
-public class CompanyApplication {
+public class SmsApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CompanyApplication.class,args);
+        SpringApplication.run(SmsApplication.class,args);
     }
 }
