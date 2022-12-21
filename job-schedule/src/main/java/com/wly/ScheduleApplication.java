586e2ce0e775ad0d1b2038b03807a514be388f70
wanglinyuan <w956322363@163.com>
2022-12-21 23:03:17 +0800

    新增定时服务

---
+++ job-schedule/src/main/java/com/wly/ScheduleApplication.java
@@ -0,0 +1,18 @@
+package com.wly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableScheduling
+public class ScheduleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleApplication.class,args);
+    }
+}
