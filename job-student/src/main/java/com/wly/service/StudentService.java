dd08e65842280852f911d957fba299ee92e25320
wanglinyuan <w956322363@163.com>
2022-12-28 23:21:41 +0800

    学生服务：服务接口及业务实现类

---
+++ job-student/src/main/java/com/wly/service/StudentService.java
@@ -0,0 +1,56 @@
+package com.wly.service;
+
+import com.wly.entity.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+/**
+ * (Student)表服务接口
+ *
+ * @author makejava
+ * @since 2022-12-26 23:03:32
+ */
+public interface StudentService {
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    Student queryById(Integer id);
+
+    /**
+     * 分页查询
+     *
+     * @param student 筛选条件
+     * @param pageRequest      分页对象
+     * @return 查询结果
+     */
+    Page<Student> queryByPage(Student student, PageRequest pageRequest);
+
+    /**
+     * 新增数据
+     *
+     * @param student 实例对象
+     * @return 实例对象
+     */
+    Student insert(Student student);
+
+    /**
+     * 修改数据
+     *
+     * @param student 实例对象
+     * @return 实例对象
+     */
+    Student update(Student student);
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param id 主键
+     * @return 是否成功
+     */
+    boolean deleteById(Integer id);
+
+}
---
+++ job-student/src/main/java/com/wly/service/impl/StudentServiceImpl.java
@@ -0,0 +1,82 @@
+package com.wly.service.impl;
+
+import com.wly.entity.Student;
+import com.wly.dao.StudentDao;
+import com.wly.service.StudentService;
+import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+
+import javax.annotation.Resource;
+
+/**
+ * (Student)表服务实现类
+ *
+ * @author makejava
+ * @since 2022-12-26 23:03:32
+ */
+@Service("studentService")
+public class StudentServiceImpl implements StudentService {
+    @Resource
+    private StudentDao studentDao;
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    @Override
+    public Student queryById(Integer id) {
+        return this.studentDao.queryById(id);
+    }
+
+    /**
+     * 分页查询
+     *
+     * @param student 筛选条件
+     * @param pageRequest      分页对象
+     * @return 查询结果
+     */
+    @Override
+    public Page<Student> queryByPage(Student student, PageRequest pageRequest) {
+        long total = this.studentDao.count(student);
+        return new PageImpl<>(this.studentDao.queryAllByLimit(student, pageRequest), pageRequest, total);
+    }
+
+    /**
+     * 新增数据
+     *
+     * @param student 实例对象
+     * @return 实例对象
+     */
+    @Override
+    public Student insert(Student student) {
+        this.studentDao.insert(student);
+        return student;
+    }
+
+    /**
+     * 修改数据
+     *
+     * @param student 实例对象
+     * @return 实例对象
+     */
+    @Override
+    public Student update(Student student) {
+        this.studentDao.update(student);
+        return this.queryById(student.getId());
+    }
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param id 主键
+     * @return 是否成功
+     */
+    @Override
+    public boolean deleteById(Integer id) {
+        return this.studentDao.deleteById(id) > 0;
+    }
+}
