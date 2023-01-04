a65ddde19778a676d2435e8a6f59244abc71c332
wanglinyuan <w956322363@163.com>
2023-01-04 22:31:04 +0800

    fix:数据测试

--- job-student/src/test/java/com/wly/StudentApplicationTest.java
+++ job-student/src/test/java/com/wly/StudentApplicationTest.java
@@ -1,7 +1,10 @@
 package com.wly;
 
+import com.wly.entity.Company;
 import com.wly.entity.Student;
 import com.wly.mapper.StudentMapper;
+import com.wly.service.StudentService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runner.Runner;
@@ -11,13 +14,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 import java.util.List;
-
+import java.util.Map;
+@Slf4j
 @SpringBootTest(classes = com.wly.StudentApplication.class)
 @RunWith(SpringRunner.class)
 public class StudentApplicationTest {
     @Autowired
     public StudentMapper studentMapper;
-
+    @Autowired
+    public StudentService studentService;
     @Test
     public void test(){
 //        long count = studentMapper.count();
@@ -32,13 +37,21 @@ public class StudentApplicationTest {
 //        for (Student student : students) {
 //            System.out.println("学生"+student.getId()+"信息："+student);
 //        }
-        Student student = new Student();
-        student.setId(4);
+//        Student student = new Student();
+//        student.setId(4);
 //        student.setName("徐云");
 //        student.setSex("男");
-        student.setPassword("123456");
-        int res = studentMapper.update(student);
+//        student.setPassword("123456");
+//        int res = studentMapper.update(student);
 //        System.out.println("插入记录数"+res);
-        System.out.println("更新记录数"+res);
+//        System.out.println("更新记录数"+res);
+
+        Map<String, Object> map = this.studentService.queryByNamePage(1, 5,"张");
+        long total = (long) map.get("total");
+        log.info("名字张的相关记录数：{}", total);
+        List<Student> students = (List<Student>) map.get("students");
+        for (Student student : students) {
+            log.info("学生对象{}的信息：{}",student.getId(),student);
+        }
     }
 }
