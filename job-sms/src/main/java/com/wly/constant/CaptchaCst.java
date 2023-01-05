c9a08f6f4f80fbe4007004460129aad9bb92a5d7
wanglinyuan <w956322363@163.com>
2023-01-06 00:32:05 +0800

    短信服务：自定义短信异常

---
+++ job-sms/src/main/java/com/wly/constant/CaptchaCst.java
@@ -0,0 +1,12 @@
+package com.wly.constant;
+
+public interface CaptchaCst {
+    /**
+     * 过期验证码key
+     */
+    String TIMEOUT_KEY = "TIMEOUT_";
+    /**
+     * 用户手机phone_key
+     */
+    String PHONE_KEY = "PHONE_";
+}
